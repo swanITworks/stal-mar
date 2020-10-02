28393a63a42789937a5d7caad07fd8be0903f0e8
swanITworksl <wm_swan@icloud.com>
2020-10-02 11:11:22 +0200

    connect with contentful headless cms

--- src/components/Banner/HelloSection/HelloSection.tsx
+++ src/components/Banner/HelloSection/HelloSection.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import * as styles from './HelloSection.module.scss'
+import { graphql, useStaticQuery } from 'gatsby'
+
+const getData = graphql`
+  {
+    hello: contentfulHelloSection {
+      spanTitle
+      helloText
+    }
+  }
+`
 
 const HelloSection = (): JSX.Element => {
+  const {
+    hello: { spanTitle, helloText },
+  } = useStaticQuery(getData)
+
   return (
     <section className={styles.helloSection}>
       <article>
-        <h3>Solidne</h3>
-        <h1>Bramy i ogrodzenia Piła</h1>
+        <h3>{spanTitle}</h3>
+        <h1>{helloText}</h1>
       </article>
     </section>
   )
